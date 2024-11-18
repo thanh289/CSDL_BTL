ff141c82a091dce1afcc853f29e02304340bb13c
thanh289 <tranduythanh2809@gmail.com>
2024-11-19 00:01:34 +0700

    add pagination for search

--- search.php
+++ search.php
@@ -1,15 +1,58 @@
 <link rel="stylesheet" type="text/css" href="css/search.css" />
 <div class="prd-block">
-<?php
-    if(isset($_POST['stext'])){
-        $stext = $_POST['stext'];
-    }else{
-        $stext = '';
-    }
-    $newStext = str_replace(' ', '%', $stext);
-    $sql = "SELECT * FROM product WHERE productName LIKE '%$newStext%'";
-    $result = $conn->query($sql);
-?>
+    <?php
+        if(isset($_POST['stext'])){
+            $stext = $_POST['stext'];
+        }else{
+            $stext = '';
+        }
+        $newStext = str_replace(' ', '%', $stext);
+
+
+        
+        // Pagination go here
+        $rowPerPage = 4;
+        if(isset($_GET['page'])){
+            $page = $_GET['page'];
+        }else{
+            $page = 1;
+        }
+
+        $perRow = $page * $rowPerPage - $rowPerPage;
+        $sql = "SELECT * FROM product WHERE productName LIKE '%$newStext%' LIMIT $perRow, $rowPerPage";
+        $result = $conn->query($sql);
+        $totalRow = mysqli_num_rows($conn->query("SELECT * FROM product WHERE productName LIKE '%$newStext%'"));
+        $totalPage = Ceil($totalRow/$rowPerPage);
+        $listPage = '';
+
+        
+        if($page>1){
+            // Return to first page
+            $listPage .= '<a href="index.php?page_layout=search&stext='.$stext.'&page=1"> << </a>';
+            // Return to previous page
+            $prev = $page-1;
+            $listPage .= '<a href="index.php?page_layout=search&stext='.$stext.'&page='.$prev.'"> < </a>';
+        }
+
+        for($i=1;$i<=$totalPage;$i++){
+            if($i==$page){
+                $listPage .=  '<span> '.$i.' </span>';
+            }else{
+                $listPage .= '<a href="index.php?page_layout=search&stext='.$stext.'&page='.$i.'"> '.$i.' </a>';
+            }
+        }
+
+        if($page<$totalPage){
+            // Go to next page
+            $next = $page+1;
+            $listPage .= '<a href="index.php?page_layout=search&stext='.$stext.'&page='.$next.'"> > </a>';
+            // Go to last page
+            $listPage .= '<a href="index.php?page_layout=search&stext='.$stext.'&page='.$totalPage.'"> >> </a>';
+           
+        }
+
+    ?>
+
 	<h2>Kết quả tìm được với từ khóa <span class="skeyword">"<?php echo $stext ?>"</span></h2>
     <div class="pr-list">
     <?php
@@ -18,7 +61,7 @@
     ?>
             <div class="prd-item">
                 <a href="index.php?page_layout=productDetail&productId=<?php echo $row['productId'] ?>">
-                    <img width="80" height="144" src="admin/image/<?php echo $row['productImage'] ?>" />
+                    <img width="208" height="200" src="admin/image/<?php echo $row['productImage'] ?>" />
                 </a>
                 <h3>
                     <a href="index.php?page_layout=productDetail&productId=<?php echo $row['productId'] ?>">
@@ -32,12 +75,14 @@
             </div>
     <?php
             $i++;
-            if($i%3==0){
+            if($i%4==0){
                 echo ' <div class="clear"></div>';
             }
         }
     ?>
-       
+       <div class="clear"></div>
     </div>
 </div>
+
+<div id="pagination"><?php echo $listPage ?></div>
 
